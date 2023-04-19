db2d6b77de3d98159ae9dc7abd65520ea2b38050
Danizza20 <125672385+Danizza20@users.noreply.github.com>
2023-04-19 08:54:56 -0600

    Add files via upload

---
+++ operamate.js
@@ -0,0 +1,40 @@
+
+function suma(){
+    var numero1, numero2;
+    numero1=prompt("Escribe un numero","");
+    numero2=prompt("Escribe otro numero","");
+    resultado=parseInt(numero1)+parseInt(numero2);
+    alert("La suma es "+resul);
+}
+
+function resta(){
+    var numero1, numero2;
+    numero1=prompt("Escribe un numero","");
+    numero2=prompt("Escribe otro numero","");
+    resultado=parseInt(numero1)-parseInt(numero2);
+    alert("La resta es "-resultado);
+}
+
+function multiplica(){
+    var numero1, numero2;
+    numero1=prompt("Escribe un numero","");
+    numero2=prompt("Escribe otro numero","");
+    resultado=parseInt(numero1)*parseInt(numero2);
+    alert("La multiplicacion es "*resultado);
+}
+
+function divide(){
+    var numero1, numero2;
+    numero1=prompt("Escribe un numero","");
+    numero2=prompt("Escribe otro numero","");
+    resultado=parseInt(numero1)/parseInt(numero2);
+    alert("La division es "/resultado);
+}
+
+function postonega(){
+    var num1, num2, resul;
+    num1=prompt("Escribe un numero","");
+    num2=prompt("Escribe otro numero","");
+    resul=parseInt(num1)+parseInt(num2);
+    alert("La suma es "+resul);
+}
