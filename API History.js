162e49cba2d18911a0617279958357b0b002e005
Danizza20 <125672385+Danizza20@users.noreply.github.com>
2023-05-09 08:39:28 -0600

    Add files via upload

---
+++ API History.js
@@ -0,0 +1,10 @@
+// Función para ir atrás en el historial
+function goBack() {
+    window.history.back();
+  }
+  
+  // Función para ir adelante en el historial
+  function goForward() {
+    window.history.forward();
+  }
+  
